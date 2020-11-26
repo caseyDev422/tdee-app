fd0ec87ca19d2feb9366070c04a16d0b99c616f8
Sean Casey <seancasey076@gmail.com>
2020-11-26 12:03:34 -0600

    Added picture and started tdee description

--- TDEE_calculator2/app/src/main/java/com/example/tdee_calculator/TdeeInformation.java
+++ TDEE_calculator2/app/src/main/java/com/example/tdee_calculator/TdeeInformation.java
@@ -1,6 +1,7 @@
 package com.example.tdee_calculator;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +10,10 @@ public class TdeeInformation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tdee_info);
+
+        TextView info = findViewById(R.id.txtInfo);
+
+        info.setText("TDEE stands for Total Daily Energy Expenditure. The TDEE is composed of three parts:\n" +
+                "BMR-Basal Metabolic Rate\n TEF-Thermic effect of Food\n Physical activity including exercise and NEAT\n");
     }
 }
